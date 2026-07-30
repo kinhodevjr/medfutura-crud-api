b6469f06d02b10f59553975c418ff934cade0c11
kinhodevjr <kinho1206@bol.com.br>
2026-07-30 17:55:34 -0300

    Feat: implementado busca por ID

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,4 +4,6 @@ const app = express();
 app.use(express.json());
 
 app.use(require("./routes/criar_user"));
+app.use(require("./routes/Buscar_pessoa"));
+
 module.exports = app;
--- backend/src/controllers/buscar_pessoaController.js
+++ backend/src/controllers/buscar_pessoaController.js
@@ -1,4 +1,17 @@
+const db = require("../database/connection")
+
 async function buscar_id(req, res) {
+    let id = req.params.id
+    const resposta = await db.query(`SELECT * FROM pessoas WHERE id = $1`,[id])
+
+    if (resposta.rows.length === 0){
+        return res.status(404).send("Id nao encontrado!")
+    }
+
+    return res.status(200).json({
+        mensagem: resposta.rows
+    })
+    
     
 }
 
