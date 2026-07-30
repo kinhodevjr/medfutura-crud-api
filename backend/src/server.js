c7c307a48567fbfb3bd1705cad4024253e0425f6
kinhodevjr <kinho1206@bol.com.br>
2026-07-30 14:07:17 -0300

    feat: credenciais do banco, express rodando e db rodando

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const app = require("./app"); 
 const PORT = process.env.PORT || 3000;
+const pool = require("./database/connection");
+pool.connect().then(()=>console.log("banco de dados conectado!")).catch((err)=>console.log(err))
 
 app.listen(PORT, () => {
     console.log('Servidor Iniciado, rodando na porta ${PORT}')
