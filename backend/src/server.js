690c7f8d19e68a956c25eae17870f7a2606833cd
kinhodevjr <kinho1206@bol.com.br>
2026-07-30 14:22:25 -0300

    fix: criação do readme, correções de sintaxes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,8 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 const pool = require("./database/connection");
 
-pool.connect().then(()=>console.log("banco de dados conectado!")).catch((err)=>console.log(err));
+pool.connect().then(()=>console.log("banco de dados conectado!"))
+              .catch((err)=>console.log(err));
 
 app.listen(PORT, () => {
     console.log(`Servidor Iniciado, rodando na porta ${PORT}`)
