5a104fefe36d1f50861c607b6a7de69a8fae1674
kinhodevjr <kinho1206@bol.com.br>
2026-07-30 21:42:08 -0300

    fix: correção de url

--- backend/src/routes/atualizar_pessoa.js
+++ backend/src/routes/atualizar_pessoa.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const update_person = require("../controllers/atualizar_pessoaController");
 
-router.put("/pessoa/:id", update_person.atualizar_pessoa);
+router.put("/pessoas/:id", update_person.atualizar_pessoa);
 
 module.exports = router;
